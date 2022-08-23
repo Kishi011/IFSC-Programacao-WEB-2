0645f281742a48b2af2fc8178c411191e0d1614a
Kishi011 <110635948+Kishi011@users.noreply.github.com>
2022-08-22 21:38:57 -0300

    Add files via upload

--- formsAvaliacao/valida.php
+++ formsAvaliacao/valida.php
@@ -25,89 +25,95 @@ $conhecimentos = $_POST['conhecimentos'];
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
-        <div class="container">
+        <div class="container pt-3 pb-3">
             <h1>Currículo enviado</h1>
             <p>
-                <h3>Dados</h3>
+                <h3>Ficha Técnica - Candidato: <?php echo $nome." ".$sobrenome;?></h3>
             </p>
-            <form class="row g-3 needs-validation" method="POST" action="valida.php">
+            <form action="index.html">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <td>Nome:</td>
+                            <td>Sobrenome:</td>
+                            <td>Email:</td>
+                            <td>Sexo:</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $nome;?></td>
+                            <td><?php echo $sobrenome;?></td>
+                            <td><?php echo $email;?></td>
+                            <td><?php echo $sexo;?></td>
+                        </tr>
+                    </tbody>
+                    <thead><td></td></thead>
+                    <thead>
+                        <tr>
+                            <td>CPF:</td>
+                            <td>Cidade:</td>
+                            <td>Estado:</td>
+                            <td>Endereço:</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $cpf;?></td>
+                            <td><?php echo $cidade;?></td>
+                            <td><?php echo $estado;?></td>
+                            <td><?php echo $endereco;?></td>
+                        </tr>
+                    </tbody>
+                    <thead><td></td></thead>
+                    <thead>
+                        <tr>
+                            <td>CEP:</td>
+                            <td>Tel:</td>
+                            <td>Escolaridade:</td>
+                            <td>Outros cursos:</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $cep?></td>
+                            <td><?php echo $fone;?></td>
+                            <td><?php echo $escolaridade;?></td>
+                            <td><?php if(empty($cursos)){
+                                echo "Não";
+                            }
+                            else{
+                                echo $cursos;
+                            }
+                                ?>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <thead><td></td></thead>
+                    <thead>
+                        <tr>
+                            <td>Experiência de Trabalho:</td>
+                            <td>Conhecimentos:</td>
+                        </tr>   
+                    </thead>
+                    <tbody>
+                        <td><?php if(empty($experiencia)){
+                            echo "Sem experência prévia de trabalho";
+                        }
+                        else{
+                            echo $experiencia;
+                        }
+                            ?>
+                        </td>
+                        <td><?php echo $conhecimentos?></td>
+                        <td></td>
+                        <td></td>
+                    </tbody>
+                </table>
                 <div class="row g-2">
-                    <div class="col-md-4">
-                        <label for="nome" class="form-label">Nome:</label>
-                        <input class="form-control" value=<?php echo $nome?>>
-                    </div>
-                    <div class="col-md-4">
-                        <label for="sobrenome" class="form-label">Sobrenome:</label>
-                        <input class="form-control" value=<?php echo $sobrenome?>>
-                    </div>
-                    <div class="col-md-3">
-                        <label for="cpf" class="form-label"><abbr>CPF:</abbr></label>
-                        <input class="form-control" value=<?php echo $cpf?>>
-                    </div>
+                    <button type="submit" class="col-sm-1 btn bg-primary text-white m-1">Voltar</button>
                 </div>
-                <div class="row g-2">
-                    <div class="col-md-5">
-                        <label for="email" class="form-label">Email:</label>
-                        <input class="form-control" value=<?php echo $email?>>
-                    </div>
-                    <div class="col-sm-3">
-                        <label class="form-label">Sexo:</label>
-                        <input class="form-control" value=<?php echo $sexo ?>>
-                    </div>
-                </div>
-                <div class="row g-2">
-                    <div class="col-md-4">
-                        <label class="form-label">Cidade:</label>
-                        <input type="text" class="form-control" id="cidade" name="cidade">
-                    </div>
-                    <div class="col-md-3">
-                        <label class="form-label">Estado:</label>
-                        <select class="form-select">
-                            <option value="SC">Santa Catarina</option>
-                            <option value="PR">Paraná</option>
-                            <option value="RS">Rio Grande do Sul</option>
-                        </select>
-                    </div>
-                    <div class="col-md-5">
-                        <label class="form-label">Endereço:</label>
-                        <input type="text" placeholder="Ex: Rua do meu Bairro n° XXX-X" class="form-control" id="endereco" name="endereco">
-                    </div>
-                    <div class="col-md-3">
-                        <label class="form-label">CEP:</label>
-                        <input type="text" class="form-control" id="cep" name="cep">
-                    </div>
-                    <div class="col-md-3">
-                        <label class="form-label">Telefone de Contato:</label>
-                        <input type="text" class="form-control" id="telefone" name="telefone">
-                    </div>
-                </div>
-                <div class="row g-2">
-                    <h2>Escolaridade</h2>
-                    <div class="col-md-3">
-                        <label class="form-label">Grau de escolaridade:</label>
-                        <select class="form-select">
-                            <option value="EnsFundComp">Ensino Fundamental Completo</option>
-                            <option value="EnsMedIncomp">Ensino Médio Incompleto</option>
-                            <option value="EnsMedComp">Ensino Médio Completo</option>
-                            <option value="EnsSupComp">Ensino Superior Completo</option>
-                            <option value="PosGrad">Pós-Graduação</option>
-                            <option value="Doutorado">Doutorado</option>
-                        </select>
-                    </div>
-                    <div class="col-lg-">
-                        <label class="form-label">Outros cursos (Opcional):</label>
-                        <textarea class="form-control" rows="3" id="cursos"></textarea>
-                    </div>
-                </div>
-                <div class="row g-2">
-                    <h2>Experiência de trabalho</h2>
-                    <textarea class="form-control" rows="5" id="experiencia"></textarea>
-                </div>
-                <div class="row g-2">
-                    <h2>Conhecimentos</h2>
-                    <textarea class="form-control" rows="5" id="conhecimentos"></textarea>
-                </div>
-              </form>
+            </form>
         </div>
     </body>
 </html>
